ffdf8f5ddd121df214133e16d4d3fb4cee567c99
jumpeebunee <jumpeebunee@gmail.com>
2022-11-16 13:55:27 +0300

    feat: add nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navigation/>
       <AppRouter/>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <header className='header'>
+        <div className='container header__navigation'>
+            <Link className='header__logo' to='/'>JumpeeBunee</Link>
+            <nav className='header__nav'>
+                <ul>
+                    <li><a className='header__nav-link header__nav-link_active' href='#home'>Home</a></li>
+                    <li><Link className='header__nav-link' to="about">About</Link></li>
+                    <li><a className='header__nav-link' href='#works'>Works</a></li>
+                    <li><a className='header__nav-link' href='#contact'>Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  );
+};
+
+export default Navigation;
